3befeed0aeadf44aa18d98067911cb4e987f31fc
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-09-22 23:27:17 -0400

    add initialDisplay function
    this function is for the initial display of students

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var $studentsList = $('.student-list');
-
+var studentsNumber;
 //var student = $studentsList.children().first();
 //student.css('display', 'none');
 var mainPage = $('.page');
@@ -7,11 +7,20 @@ var list = $('<ul></ul>').addClass('pagination');
 //list.addClass('adasa');
 mainPage.append(list);
 
+function inititalDisplay(){
+    var counter = 0;
+    for(var i=0; i<$studentsList.children().length; i++){
+        $('.student-list li').css('display', 'block');
+        counter++;
+    }
+    
+    studentsNumber = counter;
+    createPages(studentsNumber);
+}
 
 
-
-function createPages(){
-    var studentsNumber = $studentsList.children().length;
+function createPages(studentsNumber){
+    //var studentsNumber = $studentsList.children().length;
     var pagesNumber = Math.ceil(studentsNumber/10);
     //console.log(pagesNumber);
     var pagesList = $('<ul></ul>').addClass('pagination');
@@ -23,6 +32,8 @@ function createPages(){
     }
 }
 
-createPages();
+
+
+
 
 
